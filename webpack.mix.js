a862f2487a69d08695729aa9b5f63e0feea52163
thracefields <thracefields@gmail.com>
2018-05-14 14:26:58 +0300

    Change webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,13 +17,4 @@ mix.disableNotifications();
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.scripts([ 
-    
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    'node_modules/fullcalendar/dist/fullcalendar.min.js',
-    'node_modules/fullcalendar/dist/locale/bg.js',
-    'node_modules/parsleyjs/dist/parsley.min.js',
-    'node_modules/parsleyjs/dist/i18n/bg.js'
-], 'public/js/vendor.js');
-
 mix.browserSync('localhost:8000');
